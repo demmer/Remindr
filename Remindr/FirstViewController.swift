dc500d33b761b16b7cfe45c70c80901f2275d907
Michael Demmer <demmer@jut.io>
2016-04-20 09:52:37 -0700

    Initial Commit

---
+++ Remindr/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Remindr
+//
+//  Created by Michael Demmer on 4/20/16.
+//  Copyright © 2016 Michael Demmer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
