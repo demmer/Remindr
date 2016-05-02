4c6527d8103917b40446926ab1c8e1262cf0ef75
Michael Demmer <demmer@jut.io>
2016-05-02 14:25:37 -0700

    basic fetching of events working and rendering to the table

--- Remindr/FirstViewController.swift
+++ Remindr/FirstViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Michael Demmer. All rights reserved.
 //
 
+import EventKit
 import UIKit
 
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
@@ -13,10 +14,40 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
 
     // MARK: properties
     
-    var items: [String] = ["Hello", "World"];
+    var items: [EKReminder] = [];
+    var eventStore: EKEventStore = EKEventStore.init();
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.getEvents()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.getEvents()
+    }
+    
+    func getEvents() {
+        let calendars = self.eventStore.calendarsForEntityType(EKEntityType.Reminder);
+        let eventsPredicate = self.eventStore.predicateForEventsWithStartDate(
+            NSDate.distantPast(), endDate: NSDate.distantFuture(), calendars: calendars);
+        let remindersPredicate = self.eventStore.predicateForRemindersInCalendars(calendars);
+        self.eventStore.requestAccessToEntityType(EKEntityType.Reminder) { (granted, err) in
+            if (!granted) {
+                print("access denied");
+                return;
+            }
+            
+            let events = self.eventStore.fetchRemindersMatchingPredicate(remindersPredicate, completion: {
+                (reminders: [EKReminder]?) in
+                print("got reminders", reminders!.count)
+                self.items.removeAll();
+                self.items.appendContentsOf(reminders!);
+                self.tableView.reloadData()
+            });
+        }
+        self.eventStore.requestAccessToEntityType(EKEntityType.Event) { (granted, err) in
+            print("event access granted", granted, err);
+        }
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
@@ -26,14 +57,16 @@ class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("count", self.items.count)
         return self.items.count;
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell? = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell?
         
-        cell?.textLabel?.text = self.items[indexPath.row]
+        cell?.textLabel?.text = self.items[indexPath.row].title
         
+        print("cell", indexPath, cell?.textLabel?.text)
         return cell!
     }
     
